565443339350073cec7ca68132b4bbdb71126e2a
Patrick Florek <patrick.florek@gmail.com>
2022-01-31 20:57:31 +0100

    feat: add auto merge job if mergify is disabled

--- src/awscdk-app-ts.ts
+++ src/awscdk-app-ts.ts
@@ -1,4 +1,5 @@
 import { awscdk, javascript, Task } from "projen";
+import { Job, JobPermission } from "projen/lib/github/workflows-model";
 
 export interface AwsCdkTypeScriptAppOptions extends awscdk.AwsCdkTypeScriptAppOptions {}
 
@@ -62,5 +63,45 @@ export class AwsCdkTypeScriptApp extends awscdk.AwsCdkTypeScriptApp {
     });
 
     this.jest?.addTestMatch("<rootDir>/**/?(*.)@(spec|test).[tj]s?(x)");
+
+    if (!this.github?.mergify) {
+      const conditions: Array<string> = [];
+      conditions.push("github.event.pull_request.user.login == 'dependabot[bot]'");
+      conditions.push("contains(github.event.pull_request.labels.*.name, 'auto-approve");
+      conditions.push("-label~=(do-not-merge)");
+      conditions.push("#approved-reviews-by>=1");
+
+      const mergeJob: Job = {
+        runsOn: ["ubuntu-latest"],
+        permissions: {
+          pullRequests: JobPermission.WRITE,
+        },
+        if: conditions.join(" && "),
+        steps: [
+          {
+            uses: "actions/checkout@v2",
+          },
+          {
+            // https://github.com/pascalgn/automerge-action
+            uses: "pascalgn/automerge-action@v0.14.3",
+            env: {
+              GITHUB_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
+              MERGE_LABELS: "",
+              MERGE_FORKS: "false",
+              MERGE_RETRY_SLEEP: "60000",
+              MERGE_DELETE_BRANCH: "true",
+            },
+          },
+        ],
+      };
+
+      const workflow = this.github?.addWorkflow("auto-merge")!;
+      workflow.on({
+        pullRequestTarget: {
+          types: ["labeled", "opened", "synchronize", "reopened", "ready_for_review"],
+        },
+      });
+      workflow.addJobs({ merge: mergeJob });
+    }
   }
 }
