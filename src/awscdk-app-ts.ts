c8e3137775b105dfbfcbebf84ef62294a9d56e32
Patrick Florek <patrick.florek@gmail.com>
2022-02-21 01:56:23 +0100

    fix: auto-merge only if github

--- src/awscdk-app-ts.ts
+++ src/awscdk-app-ts.ts
@@ -64,7 +64,7 @@ export class AwsCdkTypeScriptApp extends awscdk.AwsCdkTypeScriptApp {
 
     this.jest?.addTestMatch("<rootDir>/**/?(*.)@(spec|test).[tj]s?(x)");
 
-    if (!this.github?.mergify) {
+    if (!this.github?.mergify && options.github) {
       const conditions: Array<string> = [];
       conditions.push("github.event.pull_request.user.login == 'dependabot[bot]'");
       conditions.push("contains(github.event.pull_request.labels.*.name, 'auto-approve')");
